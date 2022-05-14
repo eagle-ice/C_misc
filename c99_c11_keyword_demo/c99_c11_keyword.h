b6c9ad966ce00458e9c4dda0ab82fd92141dd10a
xiaokang.yang <xiaokang.yang@anker.com>
2022-05-14 06:43:15 -0700

    保存以下

---
+++ c99_c11_keyword_demo/c99_c11_keyword.h
@@ -0,0 +1,41 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  c99_c11_keyword.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  09/30/2021 06:35:35 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#ifndef _C99_C11_KEYWORD_H_
+#define _C99_C11_KEYWORD_H_
+
+/*******************************
+ * 以下为C99 C11 标准新增关键字
+ ******************************/
+
+/*******************************
+ * 1. inline
+ * 2. restrict
+ * 3. _Bool
+ * 4. _Complex
+ * 5. _Imagiary
+ * 6. _Alignas
+ * 7. _Alignof
+ * 8. _Atomic
+ * 9. _Static_assert
+ * 10. _Noreturn
+ * 11. _Thread_local
+ * 12. _Generic
+ *******************************/
+
+#endif //_C99_C11_KEYWORD_H_ end
+
